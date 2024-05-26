35b0917d8e617bc50caefb518a7b57326c5e49b8
BertaBugueno <163201633+BertaBugueno@users.noreply.github.com>
2024-05-26 19:31:44 -0400

    Entrega Trabajo 2 (corregido)

---
+++ tarea-3-R.R
@@ -0,0 +1,118 @@
+
+
+#tarea 2 
+
+
+#CARGAR PAQUETES##
+
+pacman::p_load(tidyverse, #Conjunto de paquetes, sobre todo dplyr y ggplot2
+               car, #Para recodificar
+               haven,
+               summarytools, #Para descriptivos
+               sjmisc,
+               psych,
+               sjlabelled,
+               sjPlot,
+               corrplot,
+               kableExtra
+
+)
+
+##CARGAR BASE DE DATOS#
+
+Base_de_datos_EBS_2021_STATA <- read_dta("Imput/Base de datos EBS 2021 STATA.dta")
+
+
+##selección de variables ##
+#Se trabajará con variables nivel educacional, satisfacción de ingresos, satisfacción de vivienda
+#educación ingresos,zona, la encuesta EBS, contiene estas variables que se corresponden 
+#lo que esta investigación se plantea dilucidar#
+
+
+sjlabelled::get_label(proc_data)
+
+#Cambiar nombres y etiquetas
+# Se siguen los pasos para el cambio de etiquetas, las que quedarán finalmente como; nivel educacional
+#satisfacción con los ingresos, satisfacción con la vivienda, efecto de la educación sobre los ingresos, zona
+proc_data = Base_de_datos_EBS_2021_STATA %>% select(e6a, a3_1, a3_3, a3_7, d1_1, zona)
+
+#e6a se toma de la base de datos del 2020 porque la del 2021 tiene muchos casos perdidos 
+proc_data = proc_data %>% rename("nivel_educ"=e6a, "satisniveleducacional"=a3_1, "satis_ingreso"=a3_3, "satis_vivienda"=a3_7, "educ_ingresos"=d1_1, "zona"=zona)
+
+proc_data$nivel_educ = set_label(x = proc_data$nivel_educ,label = "nivel educacional")
+proc_data$satisniveleducacional = set_label(x = proc_data$satisniveleducacional, label = "satisfacción con el  nivel educacional")
+proc_data$satis_ingreso = set_label(x = proc_data$satis_ingreso,label = "satisfacción con los ingresos")
+proc_data$satis_vivienda = set_label(x = proc_data$satis_vivienda,label = "satisfacción con la vivienda")
+proc_data$educ_ingresos = set_label(x = proc_data$educ_ingresos,label = "efecto de la educación sobre los ingresos")
+proc_data$zona = set_label(x = proc_data$zona,label = "zona")
+
+
+
+#TABLA DESCRIPTIVA GENERAL#
+
+
+view(dfSummary(proc_data, headings=FALSE))
+
+#OPERALIZACIÓN DE VARIABLE#
+
+frq(proc_data$satisniveleducacional)
+frq(proc_data$satis_ingreso)
+frq(proc_data$satis_vivienda)
+frq(proc_data$educ_ingresos) #tiene NA#
+
+proc_data =na.omit(proc_data)#omitir casos perdidos de toda la base (educ, ingreso era la única que tenía NA) 
+
+
+##Recodificación de variables
+
+#Variable binaria de zona
+
+frq(proc_data$zona)
+
+proc_data$zona <- car::recode(proc_data$zona, "1=0;2=1")
+
+proc_data$zona <- factor(proc_data$zona,
+                         
+                         labels=c( "Urbano",
+                                   
+                                   "Rural"),
+                         
+                         levels=c(0,1))
+
+frq(proc_data$zona)
+frq(proc_data$nivel_educ)
+
+proc_data$nivel_educ <- car::recode(proc_data$nivel_educ, "c(1,2,3,4)=1; c(5)=2; c(6,7)=3; c(8,9,10,11)=4; 
+
+                             c(12,13,14,15,16,17)=5")
+
+proc_data$nivel_educ <- factor(proc_data$nivel_educ,
+                               
+                               labels = c("Analfabeto", "Educacion especial", "Educacion basica", 
+                                          
+                                          "Educacion media", "Educacion superior"),
+                               
+                               levels = c(1, 2, 3, 4, 5))
+
+#HACER GRÁFICOS
+proc_data %>% ggplot(aes(x = nivel_educ)) + 
+  geom_bar()
+
+proc_data %>% ggplot(aes(x = satis_ingreso)) + 
+  geom_bar()
+
+proc_data %>% ggplot(aes(x = satis_vivienda)) + 
+  geom_bar()
+
+
+
+
+
+
+
+
+
+
+                              
+
+
